692e044661c7455baf2b8768f732118cdedc21ef
TheGratefulDev <ka.ling@live.com>
2018-10-01 20:43:44 -0500

    add Core Functionalities in detail activity

--- app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
+++ app/src/main/java/com/udacity/sandwichclub/DetailActivity.java
@@ -3,7 +3,10 @@ package com.udacity.sandwichclub;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
@@ -15,16 +18,44 @@ public class DetailActivity extends AppCompatActivity {
     public static final String EXTRA_POSITION = "extra_position";
     private static final int DEFAULT_POSITION = -1;
 
+    ImageView sandwichImageIv;
+    TextView sandwichImageErrorTv;
+    TextView descriptionLabelTv;
+    TextView descriptionTv;
+    TextView ingredientsLabelTv;
+    TextView ingredientsTv;
+    TextView alsoKnownLabelTv;
+    TextView alsoKnownTv;
+    TextView originLabelTv;
+    TextView originTv;
+
+    Sandwich sandwich;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
-        ImageView ingredientsIv = findViewById(R.id.image_iv);
+        //assign labels
+        descriptionLabelTv = findViewById(R.id.description_label_tv);
+        ingredientsLabelTv = findViewById(R.id.ingredients_label_tv);
+        alsoKnownLabelTv = findViewById(R.id.also_known_label_tv);
+        originLabelTv = findViewById(R.id.origin_label_tv);
+
+        //assign image view and error text view
+        sandwichImageIv = findViewById(R.id.image_iv);
+        sandwichImageErrorTv = findViewById(R.id.image_error_tv);
+
+        //assign text views
+        descriptionTv = findViewById(R.id.description_tv);
+        ingredientsTv = findViewById(R.id.ingredients_tv);
+        alsoKnownTv = findViewById(R.id.also_known_tv);
+        originTv = findViewById(R.id.origin_tv);
 
         Intent intent = getIntent();
         if (intent == null) {
             closeOnError();
+            return;
         }
 
         int position = intent.getIntExtra(EXTRA_POSITION, DEFAULT_POSITION);
@@ -36,7 +67,10 @@ public class DetailActivity extends AppCompatActivity {
 
         String[] sandwiches = getResources().getStringArray(R.array.sandwich_details);
         String json = sandwiches[position];
-        Sandwich sandwich = JsonUtils.parseSandwichJson(json);
+
+        //initial sandwich object
+        sandwich = JsonUtils.parseSandwichJson(json);
+
         if (sandwich == null) {
             // Sandwich data unavailable
             closeOnError();
@@ -44,11 +78,6 @@ public class DetailActivity extends AppCompatActivity {
         }
 
         populateUI();
-        Picasso.with(this)
-                .load(sandwich.getImage())
-                .into(ingredientsIv);
-
-        setTitle(sandwich.getMainName());
     }
 
     private void closeOnError() {
@@ -58,5 +87,62 @@ public class DetailActivity extends AppCompatActivity {
 
     private void populateUI() {
 
+        //Handle Image not load error and image url not included
+        if(!TextUtils.isEmpty(sandwich.getImage())){
+            Picasso.with(this).load(sandwich.getImage()).into(sandwichImageIv, new com.squareup.picasso.Callback() {
+                @Override
+                public void onSuccess() {
+                    sandwichImageErrorTv.setVisibility(View.INVISIBLE);
+                }
+
+                @Override
+                public void onError() {
+                    sandwichImageErrorTv.setVisibility(View.VISIBLE);
+                }
+            });
+        }else{
+            sandwichImageErrorTv.setVisibility(View.VISIBLE);
+        }
+
+        //Null Error Handle
+        if(!TextUtils.isEmpty(sandwich.getMainName())){
+            setTitle(sandwich.getMainName());
+        }
+
+        //Null Error Handle and remove Description label if description is not given
+        if(!TextUtils.isEmpty(sandwich.getDescription())){
+            descriptionTv.setText(sandwich.getDescription());
+        }else{
+            descriptionTv.setVisibility(View.GONE);
+            descriptionLabelTv.setVisibility(View.GONE);
+        }
+
+        //Null Error Handle and remove alsoKnowsAs label if alsoKnowsAs array 0
+        if(sandwich.getAlsoKnownAs()!=null && sandwich.getAlsoKnownAs().size() > 0){
+            for (String alsoKnown: sandwich.getAlsoKnownAs()){
+                alsoKnownTv.append(alsoKnown);
+            }
+        }else{
+            alsoKnownTv.setVisibility(View.GONE);
+            alsoKnownLabelTv.setVisibility(View.GONE);
+        }
+
+        //Null Error Handle and remove ingredient label if ingredient array 0
+        if(sandwich.getIngredients()!= null && sandwich.getIngredients().size()>0){
+            for (String ingredient: sandwich.getIngredients()){
+                ingredientsTv.append(ingredient);
+            }
+        }else{
+            ingredientsTv.setVisibility(View.GONE);
+            ingredientsLabelTv.setVisibility(View.GONE);
+        }
+
+        //Null Error Handle and remove origin label if origin is not given
+        if(!TextUtils.isEmpty(sandwich.getPlaceOfOrigin()) ){
+            originTv.setText(sandwich.getPlaceOfOrigin());
+        }else{
+            originTv.setVisibility(View.GONE);
+            originLabelTv.setVisibility(View.GONE);
+        }
     }
 }
--- app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
+++ app/src/main/java/com/udacity/sandwichclub/utils/JsonUtils.java
@@ -2,9 +2,52 @@ package com.udacity.sandwichclub.utils;
 
 import com.udacity.sandwichclub.model.Sandwich;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class JsonUtils {
 
     public static Sandwich parseSandwichJson(String json) {
-        return null;
+        Sandwich sandwich = null;
+        try {
+            sandwich = new Sandwich();
+            JSONObject sandwichJsonObject = new JSONObject(json);
+
+            JSONObject sandwichNameJsonObject = sandwichJsonObject.getJSONObject("name");
+            JSONArray sandwichAlsoKnowAsJsonArray = sandwichNameJsonObject.getJSONArray("alsoKnownAs");
+            JSONArray ingredientsJsonArray = sandwichJsonObject.getJSONArray("ingredients");
+
+            sandwich.setAlsoKnownAs(convertJsonArrayToStringArray(sandwichAlsoKnowAsJsonArray));
+            sandwich.setIngredients(convertJsonArrayToStringArray(ingredientsJsonArray));
+            sandwich.setMainName(sandwichNameJsonObject.getString("mainName"));
+            sandwich.setDescription(sandwichJsonObject.getString("description"));
+            sandwich.setPlaceOfOrigin(sandwichJsonObject.getString("placeOfOrigin"));
+            sandwich.setImage(sandwichJsonObject.getString("image"));
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return sandwich;
+    }
+
+    //Convert Json to String Array
+    private static List<String> convertJsonArrayToStringArray(JSONArray jsonArray){
+        List<String> stringArray = new ArrayList<>();
+
+        for(int i = 0, count = jsonArray.length(); i< count; i++) {
+            try {
+                String jsonString = jsonArray.getString(i);
+                stringArray.add(jsonString);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+        return stringArray;
     }
 }
